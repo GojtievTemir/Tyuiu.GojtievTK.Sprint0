9d5f5eff160d3b2f9f1a04dbf2758e742a5db309
Темирсолтан Гойтиев <kotik051236@gmail.com>
2026-09-08 16:37:50 +0500

    Task2

---
+++ Tyuiu.GojtievTK.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.GojtievTK.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.GojtievTK.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckMethodValid()
+        {
+            var name = "Темир";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Темир", res);
+        }
+    }
+}
---
+++ Tyuiu.GojtievTK.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.GojtievTK.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.GojtievTK.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Темир"));
+
+        }
+    }
+}
