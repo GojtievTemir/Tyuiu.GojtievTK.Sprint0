05db7d55fc55aba2be388cb7d72b945ce2773acc
Темирсолтан Гойтиев <kotik051236@gmail.com>
2026-09-08 17:07:37 +0500

    Task3

---
+++ using Tyuiu.GojtievTK.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.GojtievTK.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.GojtievTK.Sprint0.Task3.V0
+
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+
+        }
+
+    }
+}
