b6503b29763c565647276c4dda15516dab306d0e
Темирсолтан Гойтиев <kotik051236@gmail.com>
2026-09-08 17:59:30 +0500

    Task5

---
+++ Tyuiu.GojtievTK.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.GojtievTK.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.GojtievTK.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionalValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+
+        }
+        [TestMethod]
+        public void CheckSubstractionValid()
+        {
+            Assert.AreEqual(5, DataService.Substraction(10, 5));
+
+        }
+        [TestMethod]
+        public void CheckMultiplicatonValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+
+        }
+        [TestMethod]
+        public void CheckDivisionValid()
+        {
+            Assert.AreEqual(2, DataService.Division(10, 5));
+
+        }
+    }
+}
