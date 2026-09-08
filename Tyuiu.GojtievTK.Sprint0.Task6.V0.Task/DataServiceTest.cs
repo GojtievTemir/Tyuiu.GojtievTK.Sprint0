e28162b7f2eeb8b5b0f632244d233fba0d43a12c
Темирсолтан Гойтиев <kotik051236@gmail.com>
2026-09-08 18:32:00 +0500

    Task6

---
+++ Tyuiu.GojtievTK.Sprint0.Task6.V0.Task/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.GojtievTK.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.GojtievTK.Sprint0.Task6.V0.Task
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionalArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+
+        }
+
+        [TestMethod]
+        public void CheckSubstractionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubstractionArray(numbers);
+            Assert.AreEqual(-15, res);
+
+        }
+        [TestMethod]
+        public void CheckMultiArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
+
+        }
+
+    }
+}
